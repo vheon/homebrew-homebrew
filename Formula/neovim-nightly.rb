dd612a4e4c7acc5816c035c80be181fc60aa5ac1
Andrea Cedraro <vheon@spark.local>
2021-05-09 23:31:38 +0200

    Add pre-built neovim nightly formula

---
+++ Formula/neovim-nightly.rb
@@ -0,0 +1,21 @@
+class NeovimNightly < Formula
+  desc "Ambitious Vim-fork focused on extensibility and agility"
+  homepage "https://neovim.io/"
+  license "Apache-2.0"
+  url "https://github.com/neovim/neovim/releases/download/v#{version}/nvim.appimage"
+
+  bottle :unneeded
+  cxxstdlib_check :skip
+  conflicts_with "neovim", "neovim-binary"
+
+  def install
+    prefix.install Dir["*"]
+    chmod(0755, "#{prefix}/nvim.appimage")
+    bin.install_symlink("#{prefix}/nvim.appimage" => "nvim")
+  end
+
+  def caveats; <<~EOS
+    Executable is linked as "nvim".
+    EOS
+  end
+end
