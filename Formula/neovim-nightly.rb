3e39e63a6042e807adaefeee94f26c61978d459f
Andrea Cedraro <vheon@spark.local>
2021-05-09 23:43:01 +0200

    The atom :nightly does not exists

--- Formula/neovim-nightly.rb
+++ Formula/neovim-nightly.rb
@@ -3,7 +3,7 @@ class NeovimNightly < Formula
   homepage "https://neovim.io/"
   license "Apache-2.0"
   url "https://github.com/neovim/neovim/releases/download/v#{version}/nvim.appimage"
-  version :nightly
+  version "nightly"
 
   bottle :unneeded
   cxxstdlib_check :skip
