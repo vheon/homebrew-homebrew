b426a72e649254c83055a13c7ee7a02d080e32ad
Andrea Cedraro <a.cedraro@gmail.com>
2021-06-20 15:01:08 +0200

    Make neovim-nightly a HEAD only Formula

--- Formula/neovim-nightly.rb
+++ Formula/neovim-nightly.rb
@@ -2,8 +2,7 @@ class NeovimNightly < Formula
   desc "Ambitious Vim-fork focused on extensibility and agility"
   homepage "https://neovim.io/"
   license "Apache-2.0"
-  url "https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage"
-  version "nightly"
+  head "https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage"
 
   bottle :unneeded
   cxxstdlib_check :skip
