53225d887af934593170867ebcca60397dd5c833
Andrea Cedraro <a.cedraro@gmail.com>
2022-05-21 17:59:54 +0200

    Use pre-compiled binary also on macos

--- Formula/neovim-nightly.rb
+++ Formula/neovim-nightly.rb
@@ -2,19 +2,21 @@ class NeovimNightly < Formula
   desc "Ambitious Vim-fork focused on extensibility and agility"
   homepage "https://neovim.io/"
   license "Apache-2.0"
-  head "https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage"
-
-  cxxstdlib_check :skip
-  conflicts_with "neovim", "neovim-binary"
-
-  def install
-    prefix.install Dir["*"]
-    chmod(0755, "#{prefix}/nvim.appimage")
-    bin.install_symlink("#{prefix}/nvim.appimage" => "nvim")
+  if OS.mac?
+    head "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz"
+  else
+    head "https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz"
   end
 
-  def caveats; <<~EOS
-    Executable is linked as "nvim".
-    EOS
+  conflicts_with "neovim"
+  cxxstdlib_check :skip
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  def caveats
+    'Executable is linked as "nvim".'
   end
 end
