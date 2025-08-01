60d387e4ebfaf69af48e3f512a375db9fdcdcc34
Andrea Cedraro <a.cedraro@gmail.com>
2025-08-01 14:53:09 +0200

    drop intel support

--- Casks/neovim-nightly.rb
+++ Casks/neovim-nightly.rb
@@ -2,8 +2,8 @@ cask "neovim-nightly" do
   version :latest
   sha256 :no_check
 
-  arch arm: "arm64", intel: "x86_64"
-  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-#{arch}.tar.gz",
+  arch arm: "arm64"
+  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-arm64.tar.gz",
       verified: "github.com/neovim"
 
   name "Neovim"
